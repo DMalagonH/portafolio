8390e3c4ce43e04198c71b197378f951d1d285d5
DMalagonH <diegomalagonh@gmail.com>
2014-05-21 09:02:04 -0500

    Unificacion de seccion de contacto a home

--- assets/script/views/userView.js
+++ assets/script/views/userView.js
@@ -6,6 +6,9 @@ Portfolio.Views.UserView = Backbone.View.extend({
         var $career = $(".user-career");
         var $second_career = $(".user-second-career");
         var $biography = $(".user-biography");
+        var $location = $(".user-location");
+        var $mobile = $(".user-mobile");
+        var $email = $(".user-email");
         var $twitter = $(".user-twitter");
         var $github = $(".user-github");
         var $linkedin = $(".user-linkedin");
@@ -15,6 +18,9 @@ Portfolio.Views.UserView = Backbone.View.extend({
         $career.text(this.model.get("career"));
         $second_career.text(this.model.get("second_career"));
         $biography.html(this.model.get("biography"));
+        $location.text(this.model.get("location"));
+        $mobile.text(this.model.get("mobile"));
+        $email.text(this.model.get("email"));
         $twitter.attr("href", this.model.get("twitter"));
         $linkedin.attr("href", this.model.get("linkedin"));
         $github.attr("href", this.model.get("github"));
