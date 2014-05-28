736af7b90eb47fafaba1838ddcb2d220d74594a3
DMalagonH <diegomalagonh@gmail.com>
2014-05-28 13:54:46 -0500

    Evento en tags de categorias

--- assets/script/views/tagView.js
+++ assets/script/views/tagView.js
@@ -1,7 +1,7 @@
 Portfolio.Views.TagView = Backbone.View.extend({
 	tagName: "span",
 	className: "label label-primary",
-	render: function(){
+    render: function(){
 		this.$el.text(this.model.get("title"));
 		return this;
 	}
--- assets/script/views/tagsListView.js
+++ assets/script/views/tagsListView.js
@@ -2,9 +2,16 @@ Portfolio.Views.TagsListView = Backbone.View.extend({
 	render: function () {
 		this.collection.forEach(this.addOne, this);
 	},
-
 	addOne: function (tag) {
 		var tagView = new Portfolio.Views.TagView({ model: tag });
 		this.$el.append(tagView.render().el);
-	}
+	},
+    events: {
+        "click .label": "filterCategory" 
+    },
+    filterCategory: function(){
+        
+        console.log("click");
+    }
+    
 });
